aa094feb6a7b2caba46d03d8ebbc03520a94c073
ISHTIAQ KHAN <ishtiyaqkhan1198@gmail.com>
2025-11-01 21:05:15 +0500

    1

--- Signup/signup.js
+++ Signup/signup.js
@@ -1,8 +1,44 @@
-let btn=document.getElementsByClassName(".button")
+document.addEventListener("DOMContentLoaded",()=>{
 
+    const  Fname=document.getElementById("Fname");
+    const  Lname=document.getElementById("Lname");
+    const  Dob=document.getElementById("Dob")    ;
+    const  Email=document.getElementById("Email");
+    const  Pword=document.getElementById("Ps");
+    const Submit_btn=document.getElementById("Button_submit");
 
-btn.addEventListener("click",(e)=>{
+   
+
+   Submit_btn.addEventListener("click",(e)=>{
     console.log(e);
-    e.preventdefalut();
-    alert("FUCKED UP!")
+    e.preventDefault();
+
+    let Firstname=Fname.value.trim();
+    let Lastname=Lname.value.trim();
+    let Emailname=Email.value.trim();
+    let Psdname=Pword.value.trim();
+    let Dobname=Dob.value;
+
+    if(!Dobname || Firstname=="" || Lastname=="" || Emailname=="" || Psdname=="" 
+    ) {alert("FUCKEDUP!")}
+
+
+    let user={
+        id:Date.now(),
+        FirstName:Firstname,
+        LastName:Lastname,
+        Email:Emailname,
+        Pas:Psdname,
+        Dob:Dobname
+
+    };
+
+
+    console.log(user)
+   })
+
+
+
+
+
 })
