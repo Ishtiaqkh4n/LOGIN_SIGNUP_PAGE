b943dcaf04626b1d59c76adef5db4dafa22bf1d4
ISHTIAQ <ishtiyaqkhan1198@gmail.com>
2025-11-08 08:01:18 -0800

    lol

--- LOGIN/ind.js
+++ LOGIN/ind.js
@@ -1,121 +1,64 @@
 document.addEventListener("DOMContentLoaded",()=>{
-
+    
 let Btn=document.getElementById("submit_input");
 let id_input=document.getElementById("id_input");
 let ps_input=document.getElementById("ps_input");
 
 
-let Database=   JSON.parse(localStorage.getItem("Save"))  || [];
+// let Data=JSON.parse(localStorage.getItem('save'));
+let Database=localStorage.getItem("save");
+let Data=JSON.parse(Database)
 
-Database.forEach(task=>{
-    Check(task)
-    
-})
-
-Btn.addEventListener("click",(e)=>{
-    console.log(e);
-    let User_name=id_input.value.trim();
-    let Password=ps_input.value.trim();
-    if(User_name==="" || Password==="" || Password.length<8){
-       alert("Enter valid credentials (password must be at least 8 characters).");
-        return;
-    };
-
-    let Save={
-        id:Date.now(),
-        Uid:User_name,
-        psd:Password,
-
-    };
-    id_input.value="";
-    ps_input.value="";
-
-    Database.push(Save);
-    // console.log(Database);
-    local();
-     Check(Save)
-
-
-
+let Password=[];
+let User_Em=[];
+ 
+Data.forEach(Id=>{
+   Password.push(Id.Ps);
+   User_Em.push(Id.Email)
 });
 
+let Isfound=false;
 
-function Check(task){
-
-let Id=id_input.value.trim();
-let Ps=ps_input.value.trim();
-
-console.log(task);
-
-if(task.uid===Id && task.psd===Ps){
-    alert("loginedd successfully")
-
-}
+console.log(Password);
+console.log(User_Em)
 
 
+Btn.addEventListener("click",StartSomething);
 
-}
+function StartSomething(){
+    const  User_input=id_input.value.trim();
+    const  User_password=ps_input.value.trim();
+
+   if(!User_input  || !User_password)  return;
+
+  
+
+
+   for(let i=0;i<User_Em.length;i++){
+    ///preventing , to be in Email
+    for(var C of User_input){
+        if(C===',' || C=='/' || C=='') {
+            alert("Check your Email again")  
+           
+        }
+        
+    }
+    if(User_input[i]===",") return;
+    if(User_input==User_Em[i]){
+        if(User_password==Password[i]){
+            Isfound=true;
+        }
+    }
+
+   }
+
+if(Isfound){
+    alert("LOGIN SUCCESSFULLY")
+}else{alert("NOT FOUND")}
+
+};
 
 
 
-
-function local(){
-    localStorage.setItem("Save",JSON.stringify(Database))
-}
 
 })
-
-
-// let will={name:"KHAN"}
-
-// function change(str){
-//     str.name="ISHTIAQ",
-//     console.log(str.name)
-// };
-
-
-// change(will);
-// console.log(will.name);
-
-
-
-// (function () {
-//   let x = "Hello!!";  // I will invoke myself
-// })();
-
-// (function (){
-//     console.log("SO.....!");
-// })();
-
-// Qs. Create a function using the “function” keyword that takes a String as an argument &
-// returns the number of vowels in the string.
-
-
-
-// let Show=prompt("Enter something cool!")
-
-// function Myfunction(Love){
-//     let Find=Love.length;
-//     console.log(Find)
-//     // return Find;
-
-// };
-
-// Myfunction(Show);
-
-
-// Qs. For a given array of numbers, print the square of each value using the forEach loop.
-
-// let Arr=[2,3,5,6];
-
-// Arr.forEach(start=>{
-//     // console.log({start})
-//     My(start)
-
-// });
-
-// function My(start){
-//     let Get={start};
-//     console.log(Get.start*start)
-    
-// }
